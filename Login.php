24eb3b936bcdaf27cf4ac138321dd5acb8325d04
Jared Anderson <jaredanderson8@mail.weber.edu>
2022-10-18 16:50:18 -0600

    initial bootstrap setup

---
+++ Login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="stylesheets/application.css">
+  <title>RW Login page</title>
+
+</head>
+
+<body>
+  <div class="loginLogo row">
+    <div class="col">
+      <h1>Big Logo</h1>
+    </div>
+  </div>
+
+  <div class="login">
+    <div class="row">
+      <div class="col">
+        <h1 class="display-4">Login</h1>
+        <form>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+          </div>
+          <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div> 
+  </div>
+
+  <script src="javascripts/application.js"></script>
+
+</body>
+
+</html>
