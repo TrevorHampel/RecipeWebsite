f18c44d3912e75886d96789f68199e55f29426e8
Jared Anderson <jaredanderson8@mail.weber.edu>
2022-10-18 17:24:17 -0600

    fixed some of the login page stuff

--- Login.php
+++ Login.php
@@ -18,24 +18,23 @@
   </div>
 
   <div class="login">
+    <div class="row"> 
+      <h2>Login</h2>
+    </div>
     <div class="row">
       <div class="col">
-        <h1 class="display-4">Login</h1>
         <form>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email or username">
+            <a href="#">Forgot Email?</a>
           </div>
-          <div class="form-group">
+          <div class="mb-3">
             <label for="exampleInputPassword1">Password</label>
             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            <a href="#">Forgot Password?</a>
           </div>
-          <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
+          <button type="submit" class="btn btn-primary submit">Submit</button>
         </form>
       </div>
     </div> 
