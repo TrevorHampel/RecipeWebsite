e715c20e1d01e7df969e69ba12f3e089d7290e4d
Jared Anderson <jaredanderson8@mail.weber.edu>
2022-10-20 18:37:01 -0600

    base of signup page done

--- SignUp.php
+++ SignUp.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="stylesheets/application.css">
-  <title>RW Login page</title>
+  <title>RW Sign Up page</title>
 </head>
 
 <body>
@@ -17,23 +17,28 @@
 
   <div class="card login">
     <div class="row"> 
-      <h2>Login</h2>
+      <h2>Sign Up</h2>
     </div>
     <div class="row">
       <div class="mb-3">
         <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email address or username">
-        <a href="#">Forgot Email?</a>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email address">
       </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        <a href="#">Forgot Password?</a>
-      </div>
+      <div class="col">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="col">
+          <label for="exampleInputPassword1">Confirm Password</label>
+          <input type="confirmPassword" class="form-control" id="exampleInputPassword1" placeholder="Confirm">
+        </div>
+    </div>
+    <div class="row">
+      <p>Password must use 8 or more characters with a mix of letters, numbers & symbols.</p>
     </div> 
     <div class="row buttons">
       <div class="col text-start">
-        <button type="signup" class="btn btn-primary">Sign up</button>
+        <button type="login" class="btn btn-primary">&nbspLogin&nbsp</button>
       </div>
       <div class="col text-end">
         <button type="submit" class="btn btn-primary">Submit</button>
