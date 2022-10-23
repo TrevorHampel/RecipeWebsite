051f9738eaa76cb04e65158aaaca983de8d3964e
Trevor Hampel <Hampeltr@gmail.com>
2022-10-23 13:19:02 -0600

    login Stuff part two

--- T_Login.php
+++ T_Login.php
@@ -4,24 +4,35 @@ include_once "Database.php";
 $Database = new Database();
 
 class T_Login {
-
-
     function checkLogin($username, $password){
         $Database = new Database();
         $sql = "SELECT * 
         FROM user 
         WHERE User_name LIKE '" . $username . "'
         AND password LIKE '" . $password . "'";
-        $returnArray = $Database->selectAssc(sql);
-
-        if(returnArray === NULL){
-            return false;
+        $returnArray = $Database->selectAssc($sql);
+        
+        if(empty($returnArray)){
+            return "false";
         }
         else{
-            return true;
+            return "true";
         }
-        return ;
     }
 
-
+    function createAccount($username, $password, $first, $last){
+        $Database = new Database();
+        $sql = "SELECT * 
+        FROM user 
+        WHERE User_name LIKE '" . $username . "'";
+        $returnArray = $Database->selectAssc($sql);
+        if(!empty($returnArray)){
+            return "UserTaken";
+        }
+        $sql = "INSERT INTO `user` 
+        ( `first_name`, `last_name`, `User_name`, `password`) 
+        VALUES ('$first', '$last', '$username', '$password')";
+        $Database->insert($sql);
+        return ;
+    }
 }
--- javascripts/login.js
+++ javascripts/login.js
@@ -9,9 +9,39 @@ function login(){
         password: password
     }, 
     function(result){
-        if(result === false)
+        if(result == "false")
         {
             alert("Username or password is wrong");
+        }
+        else
+        {
+            window.location.href = "TrevorScript.php";
+        }
+    });
+}
+
+function signUp(){
+    window.location.href = "CreateAccount.php";
+}
+
+function CreateAccount(){
+    var first = $("#first_name_id").val();
+    var last = $("#last_name_id").val();   
+    var username = $("#username_id").val();
+    var password = $("#password_id").val();
+    
+    $.post("T_ActionHandler.php", 
+    {
+        action: "CreateAccount",
+        username: username,
+        password: password,
+        first: first,
+        last: last
+    }, 
+    function(result){
+        if(result === "UserTaken")
+        {
+            alert("Username is taken");
         }
         else
         {
