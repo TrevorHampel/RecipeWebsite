7e438170b4322e126e2d93b76c1573d5df15e261
Mark Rader <mgrader8@gmail.com>
2022-11-22 08:30:46 -0700

    finished functionality for SeachCategory

--- SearchCategory.php
+++ SearchCategory.php
@@ -1,5 +1,6 @@
 <?php
 include_once("includes/include.php");
+
 function getCategories()
 {
     $key = auth;
@@ -21,20 +22,6 @@ function getCategories()
     return $returnArr;
 }
 
-function printCategories()
-{
-    $categories = getCategories();
-    $echoVar = "<div class='card'>";
-    foreach ($categories as $cat) {
-        $echoVar .= '<div class="card-body">';
-        $echoVar .= '<h3 class="card-title">' . $cat['strCategory'] . '</h3>';
-        $echoVar .= '<img class="card-img-left border rounded" src="' . $cat['strCategoryThumb'] . '">';
-        $echoVar .= '</div>';
-    }
-    $echoVar .= '</div>';
-
-    return $echoVar;
-}
 
 function printSelect()
 {
@@ -43,13 +30,44 @@ function printSelect()
     foreach ($categories as $cat) {
         $listVar .= "<option value='" . $cat['strCategory'] . "'>" . $cat['strCategory'] . "</option>";
     }
+
     echo $listVar;
 }
 
-function showCatRecipes($catName)
+function getRecipeByCategory($catName)
 {
-    $url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=" . $catName;
+    $url = "https://www.themealdb.com/api/json/v2/" . auth . "/filter.php?c=" . $catName;
+
+    $curl_handle = curl_init();
+    curl_setopt($curl_handle, CURLOPT_URL, $url);
+    curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
+
+    $categoryJSON = curl_exec($curl_handle);
+
+    curl_close($curl_handle);
+
+    $categoryArr = json_decode($categoryJSON, true);
+
+    $returnArr = $categoryArr['meals'];
+
+    return $returnArr;
 }
+
+function printRecipeGrid($catName)
+{
+    $UserId = $_SESSION['UserID'];
+    $recipes = getRecipeByCategory($catName);
+    $echoVar = "";
+    $echoVar .= "<div class='card-body w-50'>";
+    foreach ($recipes as $r) {
+        $echoVar .= "<img class='card-img-top border rounded' src='" . $r['strMealThumb'] . "/preview' height='150px' width='150px'>";
+        $echoVar .= "<h3 class='card-title'>" . $r['strMeal'] . "</h3>";
+        $echoVar .= "<button type='button' class='btn btn-primary btn-md' onclick='addToFavoritesList(" . $r['idMeal'] . ", $UserId)'>Add to Favorites</button>";
+    }
+    $echoVar .= "</div>";
+    echo $echoVar;
+}
+
 
 ?>
 
@@ -65,17 +83,34 @@ function showCatRecipes($catName)
     <?php include_once("includes/nav.php"); ?>
     <h2>Search Categories</h2>
     <div class="d-flex align-items-center justify-content-center">
-        <label for="categories">Choose a Category:</label>
-        <select name="categories" id="categories">
-            <?php
-            printSelect();
-            ?>
-        </select>
-        <?php
 
-        //printCategories();
-        ?>
+        <form method="post" action="">
+            <label for="categories">Choose a Category:</label>
+            <select name="categories" id="categories">
+                <option value="" disable selected>Choose a Category</option>
+                <?php
+                printSelect();
+                ?>
+            </select>
+            <input type="submit" name="search" value="Search">
+        </form>
     </div>
+    <div class="d-flex align-items-center justify-content-center">
+        <div class="card align-items-center justify-content-center m-3">
+            <?php
+            if (isset($_POST['search'])) {
+                if (!empty($_POST['categories'])) {
+                    printRecipeGrid($_POST['categories']);
+                }
+            }
+            //printCategories();
+            ?>
+        </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="javascripts/favorites.js"></script>
 
 </body>
 
