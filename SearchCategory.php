5e2aa9aebe764e23ec5f9e58afa918317309a4cb
Mark Rader <mgrader8@gmail.com>
2022-11-21 18:56:50 -0700

    pls no pr for this one

--- SearchCategory.php
+++ SearchCategory.php
@@ -20,6 +20,37 @@ function getCategories()
 
     return $returnArr;
 }
+
+function printCategories()
+{
+    $categories = getCategories();
+    $echoVar = "<div class='card'>";
+    foreach ($categories as $cat) {
+        $echoVar .= '<div class="card-body">';
+        $echoVar .= '<h3 class="card-title">' . $cat['strCategory'] . '</h3>';
+        $echoVar .= '<img class="card-img-left border rounded" src="' . $cat['strCategoryThumb'] . '">';
+        $echoVar .= '</div>';
+    }
+    $echoVar .= '</div>';
+
+    return $echoVar;
+}
+
+function printSelect()
+{
+    $listVar = "";
+    $categories = getCategories();
+    foreach ($categories as $cat) {
+        $listVar .= "<option value='" . $cat['strCategory'] . "'>" . $cat['strCategory'] . "</option>";
+    }
+    echo $listVar;
+}
+
+function showCatRecipes($catName)
+{
+    $url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=" . $catName;
+}
+
 ?>
 
 <!doctype html>
@@ -32,20 +63,17 @@ function getCategories()
 
 <body>
     <?php include_once("includes/nav.php"); ?>
-
+    <h2>Search Categories</h2>
     <div class="d-flex align-items-center justify-content-center">
+        <label for="categories">Choose a Category:</label>
+        <select name="categories" id="categories">
+            <?php
+            printSelect();
+            ?>
+        </select>
         <?php
-        $categories = getCategories();
-        $echoVar = "<div class='card'>";
-        foreach ($categories as $cat) {
-            $echoVar .= '<div class="card-body">';
-            $echoVar .= '<h3 class="card-title">' . $cat['strCategory'] . '</h3>';
-            $echoVar .= '<img class="card-img-left border rounded" src="' . $cat['strCategoryThumb'] . '">';
-            $echoVar .= '</div>';
-        }
-        $echoVar .= '</div>';
 
-        echo $echoVar;
+        //printCategories();
         ?>
     </div>
 
--- viewrecipes.php
+++ viewrecipes.php
@@ -1,5 +1,6 @@
 <?php
 include("includes/include.php");
+$UserID = $_SESSION['UserID'];
 ?>
 <!doctype html>
 <html lang=en>
