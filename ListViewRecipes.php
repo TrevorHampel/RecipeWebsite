599142209ff82f06b3f83fcf50ccc3119de7e4ca
Jared Anderson <jaredanderson8@mail.weber.edu>
2022-11-22 17:18:23 -0700

    added css to serchCategory and some other cleanup

--- ListViewRecipes.php
+++ ListViewRecipes.php
@@ -17,8 +17,9 @@
         </div>
     </div>
 
-    <!-- Single example card used for testing without the php
     <h1 class="h1 text-center">10 Random Recipes</h1>
+
+    <!-- Single example card used for testing without the php
     <div class="d-flex align-items-center justify-content-center">
         <div class="card w-50 m-5 p-2 border rounded list">
             <div class="row center">
@@ -40,20 +41,22 @@
 
     <div class="d-flex align-items-center justify-content-center">
         <?php
-        //var_dump($_SESSION);
+        $UserId = $_SESSION['UserID'];
         $recipe_array = parseRecipe(getRecipeFromAPI(0));
-        //print_r($recipe_array);
         $print_var = '<div class="card w-50 m-5 p-2 border rounded list">';
-        foreach ($recipe_array as $recipe) {
-            //print_r($recipe);
+
+        foreach ($recipe_array as $recipe) 
+        {
             $print_var .= '<div class="row center">';
                 $print_var .= '<a href="!!!TODO route to viewrecipies.php page using this recepie!!!">';
                     $print_var .= '<h3 class="card-title">' . $recipe->getName() . '</h3>';
                     $print_var .= '<p class="card-text">Recepie type: ' . $recipe->getCategory() . '</p>';
 
                     $print_var .= '<img class="card-img-top border rounded" src="' . $recipe->getThumbnail() . '" alt="' . $recipe->getName() . '"></a>';
-
-                $print_var .= '<a><button type="button" class="btn btn-primary btn-md m-3">Add to Favorites</button></a>'; //still need to implement favorites
+            $print_var .= '</div>';
+        
+            $print_var .= '<div class="row center">';
+                $print_var .= "<a><button type='button' class='btn btn-primary btn-md m-3' onclick='addToFavoritesList(" . $recipe->getId() . ", $UserId)'>Add to Favorites</button></a>";
                 $print_var .= '<a href="' . $recipe->getRecipeVideo() . '"><button type="button" class="btn btn-danger btn-md m-3">Recipe Video</button></a>';
                 $print_var .= '<a href="' . $recipe->getRecipeSource() . '"><button type="button" class="btn btn-primary btn-md m-3">Recipe Source</button></a>';
             $print_var .= '</div>';
