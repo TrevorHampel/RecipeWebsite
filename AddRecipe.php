ba2009ddd3b94375ff21a21d124854a8c965fa82
Mark Rader <mgrader8@gmail.com>
2023-02-09 16:24:27 -0700

    got tinymce working on AddRecipe

--- AddRecipe.php
+++ AddRecipe.php
@@ -106,12 +106,10 @@ include("includes/include.php");
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="~/includes/tinymce/tinymce.min.js" type="text/javascript"></script>
+    <script src="includes/tinymce/tinymce.min.js" type="text/javascript"></script>
     <script>
         tinymce.init({
-            selector: 'textarea',
-            plugins: "lists",
-            menubar: "file edit format"
+            selector: 'textarea'
         });
     </script>
 </body>
