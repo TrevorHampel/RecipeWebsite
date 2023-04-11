6e18704b3f3e482432518cb91523e88e70430e66
TrevorHampel <77370080+TrevorHampel@users.noreply.github.com>
2023-04-11 13:28:53 -0600

    broken

--- T_RecipeHelper.php
+++ T_RecipeHelper.php
@@ -12,121 +12,122 @@ class T_RecipeHelper {
 
         function printRecipeCard($recipeNumber, $btnCounter, $favOrNot){
         $recipesArray = getRecipe($recipeNumber); // getRecipe returns a recipe ||| $Recipe = new Recipe(); get recipe is returning an array
-
-            $recipesArray = getRecipe($recipeNumber); // getRecipe returns a recipe ||| $Recipe = new Recipe(); get recipe is returning an array
-            if(isset($recipesArray) && !empty($recipesArray))
-            {
-                foreach ($recipesArray as $r) {
-                    // PORTION THAT CONSIST OF THE OUTER SEGMENT OF THE CARDS //
-                    
-                    <h2 class="centerText recipe-title" id="btn'.$btnCounter.'" style="display:block;" onclick="showhidecards(this.id)">'.$r->getName().'
-                    <img src="'.$r->getThumbnail().'" style="border-radius:15%; display:inline-block; width: 100px; height: 100px;">
-                </div>
-                <div class="col-md-1">
-                    <button onclick="removeFromFavorites(' . $recipeNumber. ' ,' . $_SESSION["UserID"]. ')" class="favoritesButton">Remove</button>
-                </div>
-            </div> 
-            ';
-
-            // ECHO THE IMAGE //
-            $txt .= '
-            <div id="card'.$btnCounter.'" hidden>
-            <div class="row noedge">
-            <div class="col">
-            <img src="'.$r->getThumbnail().'">
+        $txt = "";
+        
+        $recipesArray = getRecipe($recipeNumber); // getRecipe returns a recipe ||| $Recipe = new Recipe(); get recipe is returning an array
+        if(isset($recipesArray) && !empty($recipesArray))
+        {
+            foreach ($recipesArray as $r) {
+                // PORTION THAT CONSIST OF THE OUTER SEGMENT OF THE CARDS //
+                
+                $txt .= '        <h2 class="centerText recipe-title" id="btn'.$btnCounter.'" style="display:block;" onclick="showhidecards(this.id)">'.$r->getName().'
+                <img src="'.$r->getThumbnail().'" style="border-radius:15%; display:inline-block; width: 100px; height: 100px;">
             </div>
-            ';
+            <div class="col-md-1">
+                <button onclick="removeFromFavorites(' . $recipeNumber. ' ,' . $_SESSION["UserID"]. ')" class="favoritesButton">Remove</button>
+            </div>
+        </div> 
+        ';
 
-            $ingredients = $r->getIngredients();
-            
-            // <<< INGREDIENTS PRINTING SEGEMENT >>> //
-            // ECHO LIST OF INGREDIENTS //
-                    // ECHO NAME OF RECIPE //
-                    $txt .= ' 
-                    <div class="row noedge">
-                        <div class="col-md-1">
-                        </div>
-                        <div class="col-md-10">
-                            
-                            <h2 class="centerText recipe-title" id="btn'.$btnCounter.'" style="display:block;" onclick="showhidecards(this.id)">'.$r->getName().'
-                            <img src="'.$r->getThumbnail().'" style="border-radius:15%; display:inline-block; width: 100px; height: 100px;">
-                        </div>
-                        <div class="col-md-1">
+        // ECHO THE IMAGE //
+        $txt .= '
+        <div id="card'.$btnCounter.'" hidden>
+        <div class="row noedge">
+        <div class="col">
+        <img src="'.$r->getThumbnail().'">
+        </div>
+        ';
 
-                    ';
-                    if ($favOrNot == true) {
-                        $txt .= '
-                            <button onclick="removeFromFavorites(' . $recipeNumber. ' ,' . $_SESSION["UserID"]. ')" class="favoritesButton">Remove</button>
-                        ';
-                    }
-                    else {
-                        $txt .= '
-                            <a><button type="button" class="btn btn-primary btn-md m-3" onclick="addToFavoritesList(' . $recipeNumber . ', $UserId)">Add to Favorites</button></a>
-                        ';
-                    }
-                    $txt .= '
-
-                        </div>
-                    </div> 
-                    ';
-
-                    // ECHO THE IMAGE //
-                    $txt .= '
-                    <div id="card'.$btnCounter.'" hidden>
-                    <div class="row noedge">
-                    <div class="col">
-                    <img src="'.$r->getThumbnail().'">
+        $ingredients = $r->getIngredients();
+        
+        // <<< INGREDIENTS PRINTING SEGEMENT >>> //
+        // ECHO LIST OF INGREDIENTS //
+                // ECHO NAME OF RECIPE //
+                $txt .= ' 
+                <div class="row noedge">
+                    <div class="col-md-1">
                     </div>
-                    ';
-
-                    $ingredients = $r->getIngredients();
-                    
-                    // <<< INGREDIENTS PRINTING SEGEMENT >>> //
-                    // ECHO LIST OF INGREDIENTS //
-
-                    $txt .= '
-                    <div class="col">
-                    <h3>INGREDIENTS:</h2>
-                    ';
-
-                    foreach($ingredients as $k => $v)  {
-                        $txt .= '<div class="row checkbox">';
-                        $txt .= '<div class="col-1">';
-                        $txt .= '
-                            <input type="checkbox" 
-                                name="'.$k.'" 
-                                value="'.$v.'">
-                                
-                        ';
-                        $txt .= '</div>';
-                        $txt .= '<div class="col-11">';
-                            $txt .= 
-                                    "<p>" .
-                                        $k . " : " .
-                                        $v .
-                                    "</p>";
-                            $txt .= '</div>';
-                        $txt .= '</div>';
-                    }
-
-        foreach($M_Recipe->recipe_ingredient_link as $row)
-{
-            $txt .= '<div class="row checkbox">';
-            $txt .= '<div class="col-1">';
-            $txt .= '
-                <input type="checkbox" 
-                    name="'.$row->ingredient_id->ingredient_value.'" 
-                    value="'.$row->measurement_id->measurement_value. " " . $row->measurement_id->measurement_unit_id->measurement_unit_value .'">
+                    <div class="col-md-10">
                         
-            ';
+                        <h2 class="centerText recipe-title" id="btn'.$btnCounter.'" style="display:block;" onclick="showhidecards(this.id)">'.$r->getName().'
+                        <img src="'.$r->getThumbnail().'" style="border-radius:15%; display:inline-block; width: 100px; height: 100px;">
+                    </div>
+                    <div class="col-md-1">
+
+                ';
+                if ($favOrNot == true) {
+                    $txt .= '
+                        <button onclick="removeFromFavorites(' . $recipeNumber. ' ,' . $_SESSION["UserID"]. ')" class="favoritesButton">Remove</button>
+                    ';
+                }
+                else {
+                    $txt .= '
+                        <a><button type="button" class="btn btn-primary btn-md m-3" onclick="addToFavoritesList(' . $recipeNumber . ', $UserId)">Add to Favorites</button></a>
+                    ';
+                }
+                $txt .= '
+
+                    </div>
+                </div> 
+                ';
+
+                // ECHO THE IMAGE //
+                $txt .= '
+                <div id="card'.$btnCounter.'" hidden>
+                <div class="row noedge">
+                <div class="col">
+                <img src="'.$r->getThumbnail().'">
+                </div>
+                ';
+
+                $ingredients = $r->getIngredients();
+                
+                // <<< INGREDIENTS PRINTING SEGEMENT >>> //
+                // ECHO LIST OF INGREDIENTS //
+
+                $txt .= '
+                <div class="col">
+                <h3>INGREDIENTS:</h2>
+                ';
+
+                foreach($ingredients as $k => $v)  {
+                    $txt .= '<div class="row checkbox">';
+                    $txt .= '<div class="col-1">';
+                    $txt .= '
+                        <input type="checkbox" 
+                            name="'.$k.'" 
+                            value="'.$v.'">
+                            
+                    ';
+                    $txt .= '</div>';
+                    $txt .= '<div class="col-11">';
+                        $txt .= 
+                                "<p>" .
+                                    $k . " : " .
+                                    $v .
+                                "</p>";
+                        $txt .= '</div>';
+                    $txt .= '</div>';
+                }
+
+    foreach($M_Recipe->recipe_ingredient_link as $row)
+    {
+        $txt .= '<div class="row checkbox">';
+        $txt .= '<div class="col-1">';
+        $txt .= '
+            <input type="checkbox" 
+                name="'.$row->ingredient_id->ingredient_value.'" 
+                value="'.$row->measurement_id->measurement_value. " " . $row->measurement_id->measurement_unit_id->measurement_unit_value .'">
+                    
+        ';
+        $txt .= '</div>';
+        $txt .= '<div class="col-11">';
+            $txt .= 
+                    "<p>" .
+                    $row->ingredient_id->ingredient_value . " : " 
+                    .$row->measurement_id->measurement_value. " " . $row->measurement_id->measurement_unit_id->measurement_unit_value .
+                    "</p>";
             $txt .= '</div>';
-            $txt .= '<div class="col-11">';
-                $txt .= 
-                        "<p>" .
-                        $row->ingredient_id->ingredient_value . " : " 
-                        .$row->measurement_id->measurement_value. " " . $row->measurement_id->measurement_unit_id->measurement_unit_value .
-                        "</p>";
-                $txt .= '</div>';
             $txt .= '</div>';
 
                     $txt .='
